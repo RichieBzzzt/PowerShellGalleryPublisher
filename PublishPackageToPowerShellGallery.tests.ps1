4cc404ea01addfc0c8cebc21f71dec9f7ba0902a
Richie Lee <richie@bzzzt.io>
2018-10-23 22:03:50 +0100

    comment out another test

--- PublishPackageToPowerShellGallery.tests.ps1
+++ PublishPackageToPowerShellGallery.tests.ps1
@@ -1,1 +1,3 @@
 .\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "" -path ..\seasalt #-version "0.0.0.3" -psd1FileName "seasalt.psd1"
+
+#.\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "" -path ..\seasalt -version "0.0.0.3" -psd1FileName "seasalt.psd1"
