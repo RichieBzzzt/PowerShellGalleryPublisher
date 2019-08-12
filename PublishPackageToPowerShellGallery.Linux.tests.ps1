e4ae413f9fd97816a9df28efad7f5b1774f768c2
Richie Lee <richie@bzzzt.io>
2019-08-12 16:41:08 +0100

    add local path

--- PublishPackageToPowerShellGallery.Linux.tests.ps1
+++ PublishPackageToPowerShellGallery.Linux.tests.ps1
@@ -1,12 +1,13 @@
 param(
     [Switch] $publish,
     [string] $apiKey,
-    [string] $version
+    [string] $version,
+    [string] $psd1Path
 )
 
 .\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $false -path $PSScriptRoot\seasalt -whatifpublish
 
-.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifedit -version "0.0.0.5" -psd1FileName /seasalt/seasalt.psd1
+.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifedit -version "0.0.0.5" -psd1FileName (Join-Path $publish /seasalt/seasalt.psd1)
 
 .\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifpublish -Verbose
 
