1a57073b17bdc0eedd21c56002e577c5e1ebbb38
Richie Lee <richie@bzzzt.io>
2019-08-12 16:34:25 +0100

    add file path and linux tests

---
+++ PublishPackageToPowerShellGallery.Linux.tests.ps1
@@ -0,0 +1,15 @@
+param(
+    [Switch] $publish,
+    [string] $apiKey,
+    [string] $version
+)
+
+.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $false -path $PSScriptRoot\seasalt -whatifpublish
+
+.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifedit -version "0.0.0.5" -psd1FileName /seasalt/seasalt.psd1
+
+.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifpublish -Verbose
+
+if ($publish) {
+    .\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey $apiKey -setVersionNumberInManifest $true -path .\seasalt -version $version -Verbose
+}
