221aebfef12887d84de5c1efdb252078d565b3fc
Richie Lee <richie@bzzzt.io>
2019-08-12 17:06:08 +0100

    correct casing peutetre

--- PublishPackageToPowerShellGallery.Linux.tests.ps1
+++ PublishPackageToPowerShellGallery.Linux.tests.ps1
@@ -7,7 +7,7 @@ param(
 
 .\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $false -path $PSScriptRoot\seasalt -whatifpublish
 
-.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifedit -version "0.0.0.5" -psd1FileName (Join-Path $psd1Path /seasalt/seasalt.psd1)
+.\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifedit -version "0.0.0.5" -psd1FileName (Join-Path $psd1Path /seasalt/SeaSalt.psd1)
 
 .\content\PublishPackageToPowerShellGallery\PublishPackageToPowerShellGallery.ps1 -apiKey "asdf" -setVersionNumberInManifest $true -path .\seasalt -whatifpublish -Verbose
 
